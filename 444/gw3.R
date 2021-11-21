0898be27c684454ff7217617291d1a197865ec2b
Bryan Kan <kantongyi1997@outlook.com>
2021-11-21 00:04:56 -0500

    edit_444_gw3

--- 444/gw3.R
+++ 444/gw3.R
@@ -48,90 +48,24 @@ cor(universitiesData[,-c(1:2)])
 cov(universitiesData[,-c(1:2)])
 
 
-# Compute PCs on two dimensions
-pcs <- prcomp(data.frame(universitiesData$, cereals$rating))
-summary(pcs)
-
-
-
-
-# The function rot gives us the weights-the original variables contribution
-# to the principal components
-pcs$rot
-
-
-
-# Here we get the scores for the newly create principal componants
-head(scores, 5)
-
-# Scatter plot for the first vs. second principal scores
-# The scale=0 argument to biplot() ensures that the arrows are scaled
-# to biplot() represent the loadings; other values for scale give
-# slightly different biplots with different interpretations.
-biplot(pcs, scale=0)
-
-
-# To get PVE (Proportional variance explained)
-
-# Find the standard deviation of each principal component
-pcs$sdev
-
-# Find The variance explained by each principal component
-pcs.var=pcs$sdev^2
-pcs.var
-
-# Compute the proportion of variance explained by each principal component
-pve=pcs.var/sum(pcs.var)
-pve
-
-options(DECIMAL=4)
-options(scipen = 999)
-
-# We see that the first principal component explains 54.0% of the variance
-# in the data, the next principal component explains 38.7% of the variance,
-# and so forth.
-plot(pve, 
-     xlab="Principal Component",
-     ylab="Proportion of Variance Explained",
-     ylim=c(0,1),type='b')
-
-plot(cumsum(pve),
-     xlab="Principal Component",
-     ylab=" Cumulative Proportion of Variance Explained ",
-     ylim=c(0,1) ,type='b')
-
-
-write.csv(scores, file = "scores.csv")
-
-
-
-
-
-# By using na.omit(), we remove observations that contain missing values
-
-# Compute PCs on all 13 variables
-pcs13 <- prcomp(na.omit(cereals[,-c(1:3)]))
-summary(pcs13)
+universitiesDataPcs <- prcomp(na.omit(universitiesData[,-c(1:2)]))
+summary(universitiesDataPcs)
 # The rotation matrix provides the principal component loadings
 # Each column contains the corresponding principal component loading vector.
-pcs13$rot
-pcs13$rot [,1:5]
+universitiesDataPcs$rot
+universitiesDataPcs$rot [,1:5]
 
 
-
-
-biplot(pcs13, scale=0) 
-
-
+biplot(universitiesDataPcs, scale=0) 
 
 #  loadings = TRUE draws eigenvectors.
-autoplot(pcs13, loadings = TRUE, )
-autoplot(pcs13, loadings = TRUE, loadings.label = TRUE, )
+autoplot(universitiesDataPcs, loadings = TRUE, )
+autoplot(universitiesDataPcs, loadings = TRUE, loadings.label = TRUE, )
 
 
 
-screeplot(pcs13, type = "l" )
-screeplot(pcs13, type = "l", main = "Screeplot")
+screeplot(universitiesDataPcs, type = "l" )
+screeplot(universitiesDataPcs, type = "l", main = "Screeplot")
 
 # type=“b” for both points are connected by a line. 
 
@@ -141,26 +75,26 @@ screeplot(pcs13, type = "l", main = "Screeplot")
 # Compute PCs on all 13 variables after standardization
 # By using the option scale=TRUE, we scale the variables to have standard deviation one
 
-pcs.sdr <- prcomp(na.omit(cereals[,-c(1:3)]), scale. = T)
-summary(pcs.sdr)
-pcs.sdr$rot
-pcs.sdr$rot [,1:5]
-scores <- pcs.sdr$x
+universitiesDataPcs.sdr <- prcomp(na.omit(universitiesData[,-c(1:3)]), scale. = T)
+summary(universitiesDataPcs.sdr)
+universitiesDataPcs.sdr$rot
+universitiesDataPcs.sdr$rot [,1:5]
+scores <- universitiesDataPcs.sdr$x
 
-dim(pcs.sdr$x)
+dim(universitiesDataPcs.sdr$x)
 
 
 
 
 # Find the standard deviation of each principal component
-pcs.sdr$sdev
+universitiesDataPcs.sdr$sdev
 
 # Find The variance explained by each principal component
-pcs13.var=pcs.sdr$sdev^2
-pcs13.var
+universitiesDataPcs.var=universitiesDataPcs.sdr$sdev^2
+universitiesDataPcs.var
 
 # Compute the proportion of variance explained by each principal component
-pve=pcs13.var/sum(pcs13.var)
+pve=universitiesDataPcs.var/sum(universitiesDataPcs.var)
 pve
 
 options(DECIMAL=4)
